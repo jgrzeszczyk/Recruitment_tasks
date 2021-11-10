5ed34cc4014ac601460478541d1ccf98676c3c0f
jgrzeszczyk <kuba.grzeszczyk98@gmail.com>
2021-11-11 00:45:59 +0100

    Moved GUI to separated file and some code cleanups

---
+++ Task3/game_gui.py
@@ -0,0 +1,108 @@
+from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QDialog, QGroupBox, QGridLayout, QButtonGroup, QTextEdit
+from PyQt5.QtCore import Qt, QSize, QThread
+import numpy as np
+
+from Task1.traffic_lights import Worker
+from game_of_life import GameEngine
+
+
+class GUI(QDialog):
+    def __init__(self, console_logs):
+        super().__init__()
+        self.title = 'Game of Life'
+        self.left = 200
+        self.top = 200
+        self.width = 200
+        self.height = 150
+        self.engine = GameEngine('./config.json', console_logs=console_logs)
+        self.worker = Worker(self.engine.run, update_gui_func=self.update_grid_colors)
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.vLayout = QVBoxLayout()
+        self.create_grid_layout()
+        window_layout = QVBoxLayout()
+        window_layout.addWidget(self.horizontal_group_box)
+
+        self.start_button = QPushButton('Start')
+        self.btn_group = QButtonGroup()
+        self.btn_group.setExclusive(True)
+        self.btn_group.addButton(self.start_button)
+        self.btn_group.buttonClicked.connect(self.on_click)
+
+        self.control_box = QGroupBox('Control Panel')
+        control_box_layout = QGridLayout()
+        control_box_layout.addWidget(self.start_button, 0, 0)
+
+        self.info_area = QTextEdit()
+        self.info_area.setObjectName('Console logs')
+        self.info_area.setReadOnly(True)
+
+        control_box_layout.addWidget(self.info_area, 1, 0)
+        self.control_box.setLayout(control_box_layout)
+        window_layout.addWidget(self.control_box, alignment=Qt.AlignBottom)
+        self.setLayout(window_layout)
+        self.show()
+
+    def on_click(self, btn):
+        """Defines what happens when button is clicked"""
+        if btn.text() == 'Start':
+            self.start_button.setEnabled(False)
+            self.engine.running = True
+            self.compute_iteration()
+
+    def update_grid_colors(self, iter_time):
+        """Updates cells' colors according to the actual state"""
+        for row in range(self.buttons.shape[0]):
+            for col in range(self.buttons.shape[1]):
+                color = 'black' if self.engine.cells_board[row, col].state == 1 else 'white'
+                self.buttons[row, col].setStyleSheet(f'background-color : {color}')
+        self.info_area.append(f'Iter time: {iter_time} [\u03BCs]')
+        self.info_area.ensureCursorVisible()
+
+    def create_grid_layout(self):
+        """Creates grid layout for cells"""
+        # Setting layout
+        self.horizontal_group_box = QGroupBox('Current generation')
+        layout = QGridLayout()
+        layout.setVerticalSpacing(0)
+        layout.setHorizontalSpacing(0)
+
+        # Adding cells represented as non-clickable buttons to the layout
+        temp_buttons = []
+        for row in range(self.engine.cells_board.shape[0]):
+            for col in range(self.engine.cells_board.shape[1]):
+                cell = self.engine.cells_board[row, col]
+
+                btn = QPushButton()
+                btn.setFixedSize(QSize(20, 20))
+                btn_color = 'black' if cell.state == 1 else 'white'
+                btn.setStyleSheet(f'background-color : {btn_color}')
+                btn.setEnabled(False)
+                layout.addWidget(btn, cell.row, cell.col)
+                temp_buttons.append(btn)
+
+        self.buttons = np.array(temp_buttons).reshape(self.engine.cells_board.shape[0],
+                                                      self.engine.cells_board.shape[1])
+        self.horizontal_group_box.setLayout(layout)
+
+    def compute_iteration(self) -> None:
+        """Threading to avoid GUI freezing"""
+
+        # Create a QThread object
+        self.thread = QThread()
+        #  Create a worker object
+        self.worker = self.worker
+        # Move worker to the thread
+        self.worker.moveToThread(self.thread)
+
+        # Connect signals and slots
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        # Start the thread
+        self.thread.start()
--- Task3/game_of_life.py
+++ Task3/game_of_life.py
@@ -3,12 +3,9 @@ from datetime import datetime
 import json
 from copy import deepcopy
 import time
+import sys
 
-from Task1.traffic_lights import Worker
-
-from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QApplication, QDialog, QGroupBox, QGridLayout, QButtonGroup, \
-    QTextEdit
-from PyQt5.QtCore import Qt, QSize, QThread
+from PyQt5.QtWidgets import QApplication
 
 
 class Cell:
@@ -20,6 +17,7 @@ class Cell:
     def count_neighbours(self, board: np.array) -> int:
         """Counts neighbours surrounding the specific cell"""
 
+        # Periodic boundary conditions
         row1 = board.shape[0] - 1 if self.row == 0 else self.row - 1
         row3 = 0 if self.row == board.shape[0] - 1 else self.row + 1
 
@@ -35,18 +33,21 @@ class Cell:
 
 
 class GameEngine:
-    def __init__(self, config_path: str):
+    def __init__(self, config_path: str, console_logs=False):
         with open(config_path) as f:
             self.config = json.load(f)
 
         self.validate_config()
         self.init_array = np.array(self.config['initial_pose'])
+        self.refresh_rate = self.config['refresh_rate']
+
+        # Create cells board that consists of array of cells of type Cell
         self.cells_board = np.array([[Cell(row, col, state=self.init_array[row, col])
                                       for col in range(self.init_array.shape[1])]
                                      for row in range(self.init_array.shape[0])], dtype='object')
         self.temp_board = deepcopy(self.cells_board)
-        self.refresh_rate = self.config['refresh_rate']
         self.running = False
+        self.console_logs = console_logs
 
     def validate_config(self) -> None:
         """Validates game config"""
@@ -68,13 +69,13 @@ class GameEngine:
 
     def run(self, kwargs) -> None:
         """Runs the whole game"""
-        self.print_iter()
         while self.running:
             start_time = datetime.now()
             self.compute_iter()
             self.update_board()
             elapsed_time = self.measure_iter_time(start_time)
-            self.print_iter()
+            if self.console_logs:
+                self.print_iter(elapsed_time)
             time.sleep(self.refresh_rate)
             update_gui_func = kwargs['update_gui_func']
             update_gui_func(elapsed_time)
@@ -97,118 +98,23 @@ class GameEngine:
         """Measures elapsed time since start_time"""
         return (datetime.now() - start_time).microseconds
 
-    def print_iter(self) -> None:
+    def print_iter(self, elapsed_time: int) -> None:
         """Prints current iteration array"""
         board_repr = [[self.cells_board[row, col].state for col in range(self.cells_board.shape[1])]
                       for row in range(self.cells_board.shape[0])]
-        print('\n'.join([str(x) for x in board_repr]))
+        print('\n'.join([str(x) for x in board_repr]), '\n', f'Iter time: {elapsed_time}')
 
 
-class GUI(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.title = 'Game of Life'
-        self.left = 200
-        self.top = 200
-        self.width = 200
-        self.engine = GameEngine('./config.json')
-        self.height = 150
-        self.initUI()
-        self.worker = Worker(self.engine.run, update_gui_func=self.update_grid_colors)
-
-    def initUI(self):
-        self.buttons = []
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.vLayout = QVBoxLayout()
-        self.create_grid_layout()
-        window_layout = QVBoxLayout()
-        window_layout.addWidget(self.horizontal_group_box)
-
-        self.control_box = QGroupBox('Control Panel')
-        control_box_layout = QGridLayout()
-
-        self.start_button = QPushButton('Start')
-        self.btn_group = QButtonGroup()
-        self.btn_group.setExclusive(True)
-        self.btn_group.addButton(self.start_button)
-        control_box_layout.addWidget(self.start_button, 0, 0)
-
-        self.btn_group.buttonClicked.connect(self.on_click)
-
-        self.info_area = QTextEdit()
-        self.info_area.setObjectName('Console logs')
-        self.info_area.setReadOnly(True)
-
-        control_box_layout.addWidget(self.info_area, 1, 0)
-
-        self.control_box.setLayout(control_box_layout)
-        window_layout.addWidget(self.control_box, alignment=Qt.AlignBottom)
-        self.setLayout(window_layout)
-        self.show()
-
-    def on_click(self, btn):
-        if btn.text() == 'Start':
-            self.start_button.setEnabled(False)
-            self.engine.running = True
-            self.compute_iteration()
-
-    def update_grid_colors(self, iter_time):
-        for row in range(self.buttons.shape[0]):
-            for col in range(self.buttons.shape[1]):
-                color = 'black' if self.engine.cells_board[row, col].state == 1 else 'white'
-                self.buttons[row, col].setStyleSheet(f'background-color : {color}')
-        self.info_area.append(f'Iter time: {iter_time} [\u03BCs]')
-        self.info_area.ensureCursorVisible()
-
-    def create_grid_layout(self):
-        self.horizontal_group_box = QGroupBox('Current generation')
-        layout = QGridLayout()
-        layout.setVerticalSpacing(0)
-        layout.setHorizontalSpacing(0)
-
-        temp_buttons = []
-        for row in range(self.engine.cells_board.shape[0]):
-            for col in range(self.engine.cells_board.shape[1]):
-                cell = self.engine.cells_board[row, col]
-
-                btn = QPushButton()
-                btn.setFixedSize(QSize(20, 20))
-                btn_color = 'black' if cell.state == 1 else 'white'
-                btn.setStyleSheet(f'background-color : {btn_color}')
-                btn.setEnabled(False)
-                layout.addWidget(btn, cell.row, cell.col)
-                temp_buttons.append(btn)
-
-        self.buttons = np.array(temp_buttons).reshape(self.engine.cells_board.shape[0],
-                                                      self.engine.cells_board.shape[1])
-        self.horizontal_group_box.setLayout(layout)
-
-    def compute_iteration(self) -> None:
-        """Threading to avoid GUI freezing"""
-
-        # Create a QThread object
-        self.thread = QThread()
-        #  Create a worker object
-        self.worker = self.worker
-        # Move worker to the thread
-        self.worker.moveToThread(self.thread)
-
-        # Connect signals and slots
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        # Start the thread
-        self.thread.start()
+def run_gui(console_logs):
+    from game_gui import GUI
+    window = GUI(console_logs=console_logs)
 
 
 def main():
+    # TO PRINT CONSOLE LOGS CHANGE console_logs argument in run_gui method to True
     app = QApplication([])
-    window = GUI()
-    app.exec()
-
-    # game.run()
+    run_gui(console_logs=True)
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
